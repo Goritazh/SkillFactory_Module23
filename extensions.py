cc7b4b0334ba4e1c93179d80a0e046617de4145d
Arseny <arseny.uvaroff@gmail.com>
2023-08-10 02:05:17 +0400

    Финальная версия

--- extensions.py
+++ extensions.py
@@ -3,30 +3,30 @@ import json
 from config import keys
 
 
-class ConversionException(Exception):
+class APIException(Exception):
     pass
 
 
 class CryptoConverter:
     @staticmethod
-    def convert(quote: str, base: str, amount: str):
+    def get_price(quote: str, base: str, amount: str):
         if quote == base:
-            raise ConversionException(f'Невозможно перевести одинаковые валюты {base}.')
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
 
         try:
             quote_ticket = keys[quote]
         except KeyError:
-            raise ConversionException(f'Не удалось обработать валюту {base}.')
+            raise APIException(f'Не удалось обработать валюту {base}.')
 
         try:
             base_ticket = keys[base]
         except KeyError:
-            raise ConversionException(f'Не удалось обработать валюту {base}.')
+            raise APIException(f'Не удалось обработать валюту {base}.')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise ConversionException(f'Не удалось обработать количество {amount}')
+            raise APIException(f'Не удалось обработать количество {amount}')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticket}&tsyms={base_ticket}')
         total_base = json.loads(r.content)[keys[base]]
